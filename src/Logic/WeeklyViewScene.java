16851b004d22b81991e5f1a71d4dcb91eb91ef3d
BrownTrevor <tdbrown2011@hotmail.com>
2017-11-13 15:13:22 -0800

    Scroll Bar display test

--- src/Logic/WeeklyViewScene.java
+++ src/Logic/WeeklyViewScene.java
@@ -1,5 +1,6 @@
 package Logic;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javafx.scene.Scene;
@@ -21,7 +22,45 @@ public class WeeklyViewScene {
 		dayLayer.getChildren().add(new SwitchWeeksButton(1,c,"->",uf).switchButton);
 		root.getChildren().add(dayLayer);
 		HBox recipeLayer = new HBox();
-		RecipeScrollPane rsp = new RecipeScrollPane("trevor put arraylist here");
+		
+		
+		
+		//For display testing. Delete later
+		//====================================================
+		ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
+		
+		Ingredient i1 = new Ingredient("Saffron", "1Tbsp");
+		Ingredient i2 = new Ingredient("Eggs", "2");
+		ArrayList<Ingredient> iList = new ArrayList<Ingredient>();
+		ArrayList<String> sList = new ArrayList<String>();
+		sList.add("Step 1");
+		sList.add("Step 2");
+		sList.add("Step 3");
+		iList.add(i1);
+		iList.add(i2);
+		
+		Ingredient i3 = new Ingredient("Beff", "1Tbsp");
+		Ingredient i4 = new Ingredient("Corn", "2");
+		ArrayList<Ingredient> iList1 = new ArrayList<Ingredient>();
+		ArrayList<String> sList1 = new ArrayList<String>();
+		sList1.add("Step 1");
+		sList1.add("Step 2");
+		sList1.add("Step 3");
+		iList1.add(i1);
+		iList1.add(i2);
+		
+		
+		Recipe r1 = new Recipe("Saffron Eggs", "A saffron eggs recipe", 
+				iList, sList);
+		
+		Recipe r2 = new Recipe("Beef Corn", "Corn with beeff", 
+				iList1, sList1);
+		
+		recipeList.add(r1);
+		recipeList.add(r2);
+		//====================================================
+		
+		RecipeScrollPane rsp = new RecipeScrollPane(recipeList);
 		recipeLayer.getChildren().add(rsp.sp);
 		root.getChildren().add(recipeLayer);
 		return new Scene(root,400,400);

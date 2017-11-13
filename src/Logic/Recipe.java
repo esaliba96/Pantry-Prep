8bc80dcff4b2fdccab4729de05ebdea771aef270
Trevor Brown <tbrown28@calpoly.edu>
2017-11-13 13:06:00 -0800

    Adding Recipe Shell

---
+++ src/Logic/Recipe.java
@@ -0,0 +1,35 @@
+package Logic;
+
+import java.util.*;
+
+public class Recipe{
+   private ArrayList<Ingredient> ingredientList;
+   private String description;
+   private String name;
+   private ArrayList<String> instructionList;
+
+   public Recipe(String name, String description, 
+      ArrayList<Ingredient> ingredientList, ArrayList<String> instructionList){
+
+         this.name = name;
+         this.description = description;
+         this.ingredientList = ingredientList;
+         this.instructionList = instructionList;
+   }
+
+   public String getName(){
+      return name;
+   }
+
+   public String getDescription(){
+      return description;
+   }
+
+   public ArrayList<String> getInstructionList(){
+      return instructionList;
+   }
+
+   public ArrayList<Ingredient> getIngredientList(){
+      return ingredientList;
+   }
+}

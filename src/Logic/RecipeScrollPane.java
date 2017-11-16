d3967e66bad77aa2becc3e53663e5d0615147ad3
esaliba96 <elie-saliba@hotmail.com>
2017-11-16 15:40:48 -0800

    drag and drop

--- src/Logic/RecipeScrollPane.java
+++ src/Logic/RecipeScrollPane.java
@@ -2,16 +2,18 @@ package Logic;
 
 import java.util.ArrayList;
 
+import javafx.scene.Group;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class RecipeScrollPane {
 	public ScrollPane sp;
-	public RecipeScrollPane(ArrayList<Recipe> recipeList){
+	public RecipeScrollPane(ArrayList<Recipe> recipeList, MealSlotButton mb){
 		sp = new ScrollPane();
 		VBox myList = new VBox();
 		for (int i = 0;i < recipeList.size();i++){
-			myList.getChildren().add(new RecipeButton(recipeList.get(i)).recipeButton);   
+			myList.getChildren().add(new RecipeButton(recipeList.get(i), mb).recipeButton);   
 		}
 		sp.setContent(myList);
 	}
--- src/Logic/WeeklyViewScene.java
+++ src/Logic/WeeklyViewScene.java
@@ -11,6 +11,41 @@ public class WeeklyViewScene {
 	public static Scene getScene(MyCalendar c){
 		UpdateFrame uf = new UpdateFrame();
 		VBox root = new VBox();
+		//For display testing. Delete later
+		//====================================================
+		ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
+
+		Ingredient i1 = new Ingredient("Saffron", "1Tbsp");
+		Ingredient i2 = new Ingredient("Eggs", "2");
+		ArrayList<Ingredient> iList = new ArrayList<Ingredient>();
+		ArrayList<MealSlotButton> meals = new ArrayList<>();
+		ArrayList<String> sList = new ArrayList<String>();
+		sList.add("Step 1");
+		sList.add("Step 2");
+		sList.add("Step 3");
+		iList.add(i1);
+		iList.add(i2);
+
+		Ingredient i3 = new Ingredient("Beff", "1Tbsp");
+		Ingredient i4 = new Ingredient("Corn", "2");
+		ArrayList<Ingredient> iList1 = new ArrayList<Ingredient>();
+		ArrayList<String> sList1 = new ArrayList<String>();
+		sList1.add("Step 1");
+		sList1.add("Step 2");
+		sList1.add("Step 3");
+		iList1.add(i1);
+		iList1.add(i2);
+
+		Recipe r1 = new Recipe("Saffron Eggs", "A saffron eggs recipe", 
+				iList, sList);
+
+		Recipe r2 = new Recipe("Beef Corn", "Corn with beeff", 
+				iList1, sList1);
+
+		recipeList.add(r1);
+		recipeList.add(r2);
+		//====================================================
+
 		MonthLabel monthLabel = new MonthLabel(c,uf);
 		root.getChildren().add(monthLabel.monthLabel);
 		HBox dayLayer = new HBox();
@@ -21,49 +56,20 @@ public class WeeklyViewScene {
 		}
 		dayLayer.getChildren().add(new SwitchWeeksButton(1,c,"->",uf).switchButton);
 		root.getChildren().add(dayLayer);
-		HBox recipeLayer = new HBox();
+		VBox recipeLayer = new VBox();
 		VBox mealSlots = new VBox();
 		for (int i = 0; i < 3; i++){
 			MealSlotButton msb = new MealSlotButton(c,uf,i);
 			mealSlots.getChildren().add(msb.mealSlotButton);
-		}
-		//For display testing. Delete later
-		//====================================================
-		ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
-		
-		Ingredient i1 = new Ingredient("Saffron", "1Tbsp");
-		Ingredient i2 = new Ingredient("Eggs", "2");
-		ArrayList<Ingredient> iList = new ArrayList<Ingredient>();
-		ArrayList<String> sList = new ArrayList<String>();
-		sList.add("Step 1");
-		sList.add("Step 2");
-		sList.add("Step 3");
-		iList.add(i1);
-		iList.add(i2);
-		
-		Ingredient i3 = new Ingredient("Beff", "1Tbsp");
-		Ingredient i4 = new Ingredient("Corn", "2");
-		ArrayList<Ingredient> iList1 = new ArrayList<Ingredient>();
-		ArrayList<String> sList1 = new ArrayList<String>();
-		sList1.add("Step 1");
-		sList1.add("Step 2");
-		sList1.add("Step 3");
-		iList1.add(i1);
-		iList1.add(i2);
-		
-		Recipe r1 = new Recipe("Saffron Eggs", "A saffron eggs recipe", 
-				iList, sList);
-		
-		Recipe r2 = new Recipe("Beef Corn", "Corn with beeff", 
-				iList1, sList1);
-		
-		recipeList.add(r1);
-		recipeList.add(r2);
-		//====================================================
-		
-		RecipeScrollPane rsp = new RecipeScrollPane(recipeList);
-		recipeLayer.getChildren().addAll(mealSlots,rsp.sp);
-		root.getChildren().add(recipeLayer);
+			RecipeScrollPane rsp = new RecipeScrollPane(recipeList, msb);
+			if(i == 2)
+				recipeLayer.getChildren().add(rsp.sp);
+			meals.add(msb);
+		}	
+		root.getChildren().addAll(mealSlots, recipeLayer);
+
 		return new Scene(root,400,400);
 	}
 }
+
+

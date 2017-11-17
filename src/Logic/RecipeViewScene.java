5a838053a14e26d5dc9a7be3f8d689677c2bf0c6
BrownTrevor <tdbrown2011@hotmail.com>
2017-11-17 10:39:49 -0800

    Added Hboxes to the recipe viewer

--- src/Logic/RecipeViewScene.java
+++ src/Logic/RecipeViewScene.java
@@ -1,6 +1,7 @@
 package Logic;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -20,15 +21,23 @@ public class RecipeViewScene {
 	    Text description = new Text(r.getDescription());
 	    root.getChildren().add(description);
 	    
+	    HBox hbox = new HBox();
+	    VBox vbox1 = new VBox();
+	    VBox vbox2 = new VBox();
+	    
+	    root.getChildren().add(hbox);
+	    hbox.getChildren().add(vbox1);
+	    hbox.getChildren().add(vbox2);
 	    for(Ingredient i: r.getIngredientList()){
 	    	Text ingredient = new Text(i.getQuantity() + " " + i.getUnit() + i.getIngredientName());
-	    	root.getChildren().add(description);
+	    	vbox1.getChildren().add(ingredient);
 	    }
 	    
 	    int count = 0;
 	    for(String s : r.getInstructionList()){
 	    	count++;
 	    	Text instruction = new Text(count + ". " + s);
+	    	vbox2.getChildren().add(instruction);
 	    }
 	    
 	    

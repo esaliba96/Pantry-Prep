293a2ca846f1ecab774de16b3cc99698ae2c664a
BrownTrevor <tdbrown2011@hotmail.com>
2017-11-17 16:22:36 -0800

    Changed layout of recipe view

--- src/Logic/RecipeViewScene.java
+++ src/Logic/RecipeViewScene.java
@@ -1,5 +1,7 @@
 package Logic;
 
+import java.awt.Insets;
+
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -9,10 +11,12 @@ import javafx.scene.text.Text;
 
 public class RecipeViewScene {
 	
-	public static Scene getScene(MyCalendar c, Recipe r){
+	public static Scene getScene(Recipe r){
 		
 		VBox root = new VBox();
+
 		root.getChildren().add(NavigationBar.getNavigateBox());
+		root.setSpacing(15);
 
 		Text title = new Text(r.getName());
 	    title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
@@ -24,10 +28,10 @@ public class RecipeViewScene {
 	    HBox hbox = new HBox();
 	    VBox vbox1 = new VBox();
 	    VBox vbox2 = new VBox();
-	    
+	    hbox.setSpacing(20);
 	    root.getChildren().add(hbox);
-	    hbox.getChildren().add(vbox1);
 	    hbox.getChildren().add(vbox2);
+	    hbox.getChildren().add(vbox1);
 	    for(Ingredient i: r.getIngredientList()){
 	    	Text ingredient = new Text(i.getQuantity() + " " + i.getUnit() + i.getIngredientName());
 	    	vbox1.getChildren().add(ingredient);

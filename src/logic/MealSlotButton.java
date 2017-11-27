0d60675c74b458c52db97a638f5afcfc888fdfb2
BrownTrevor <tdbrown2011@hotmail.com>
2017-11-27 13:43:52 -0800

    Added action handler for the MealSLotButton

--- src/logic/MealSlotButton.java
+++ src/logic/MealSlotButton.java
@@ -2,18 +2,48 @@ package logic;
 
 import java.util.Calendar;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class MealSlotButton implements ButtonFactory{
 	public Text mealSlotButton;
 	private int meal;
+	private Recipe recipe;
 
 	//meal 0 = breakfast 1 = lunch 2 = dinner
-	public MealSlotButton(MyCalendar c, UpdateFrame uf,int meal){
-		this.meal = meal;
+	public MealSlotButton(MyCalendar c, UpdateFrame uf,int mealNum){
+		this.meal = mealNum;
 		uf.addFactory(this);
 		mealSlotButton = new Text();
+		this.recipe = null;
+		
+		
+		//Clickable Calendar (Under construction)
+		//===============================================================
+		
+
+		
+		
+		mealSlotButton.setOnMouseClicked(new EventHandler <MouseEvent>(){
+        	public void handle(MouseEvent event) {
+        		Day day = Database.getMealsFromPlanner(c.selectedDay);
+
+        		if (day != null) {
+        			recipe = day.getRecipe(meal);
+        			if(recipe != null){
+                		PantryPrep.stage.setScene(RecipeViewScene.getScene(recipe));
+            		}
+        		}
+     
+        		event.consume();
+        	}
+        });
+		//===============================================================
+
+		
 		write(c);
+
 	}
 	
 	public void write(MyCalendar c) {
@@ -26,9 +56,12 @@ public class MealSlotButton implements ButtonFactory{
 		}
 		
 		Recipe recipe = day.getRecipe(meal);
+	
+		
 		if (recipe == null) {
 			mealSlotButton.setText("Unplanned");
 		} else {
+			this.recipe = recipe;
 			mealSlotButton.setText(recipe.getName());
 		}
 	}

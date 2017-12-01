36136fb103f847642ff4eba40be754535172ae58
esaliba96 <elie-saliba@hotmail.com>
2017-11-30 18:51:36 -0800

    test suite

---
+++ src/test/ElieTestSuite.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RecipeSceneRecipeIntegrationTest.class, RecipeTest.class })
+public class ElieTestSuite {
+
+}
